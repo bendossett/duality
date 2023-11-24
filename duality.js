01f04b2f08fe6a3765ea04d485c7a24193a4ecf7
Ben <benjamin.dossett@du.edu>
2023-11-23 23:20:40 -0700

    Work on pan + zoom

--- duality.js
+++ duality.js
@@ -6,19 +6,20 @@ const CANVAS_HEIGHT = 600;
 
 const dragThreshold = 0.1;
 
-let mouseWasPressed = false;
-let mouseDownX;
-let mouseDownY;
-
 let primal;
 let dual;
 
 let drawSegments = true;
 
+let scaleFactor = 1.0;
+let offsetX = 0.0;
+let offsetY = 0.0;
+let targetOffsetX = 0.0;
+let targetOffsetY = 0.0;
+
 window.onload = () => {
     document.querySelector("#mode-select").addEventListener("click", setModeStatus);
 }
-
 
 function setModeStatus() {
     drawSegments = !drawSegments;
@@ -36,14 +37,17 @@ function map(n, start1, stop1, start2, stop2) {
 };
 
 function canvas2coord(px, py) {
-    let x = map(px, 0, CANVAS_WIDTH, 0, WIDTH);
-    let y = map(py, 0, CANVAS_HEIGHT, 0, HEIGHT);
-    return [Math.min(Math.max(x - (WIDTH / 2), -(WIDTH / 2)), (WIDTH / 2)), Math.min(Math.max(HEIGHT - y - (HEIGHT / 2), -(HEIGHT / 2)), (HEIGHT / 2))];
+    const scale = 1 / scaleFactor;
+    let x = map(px, 0, CANVAS_WIDTH, 0, (scale * WIDTH) * 2);
+    let y = map(py, 0, CANVAS_HEIGHT, 0, (scale * HEIGHT) * 2);
+    return [Math.min(Math.max((x - (scale * WIDTH)) - offsetX, -((scale * WIDTH) / 2)), ((scale * WIDTH))), 
+            Math.min(Math.max((-(y - (scale * HEIGHT))) - offsetY, -((scale * HEIGHT))), ((scale * HEIGHT)))];
 }
 
 function coord2canvas(px, py) {
-    let x = map(px + (WIDTH / 2), 0, WIDTH, 0, CANVAS_WIDTH);
-    let y = map(HEIGHT - (py + (HEIGHT / 2)), 0, HEIGHT, 0, CANVAS_HEIGHT);
+    const scale = 1 / scaleFactor;
+    let x = map((px + offsetX) + (scale * WIDTH), 0, (scale * WIDTH) * 2, 0, CANVAS_WIDTH);
+    let y = map((scale * HEIGHT) - (py + offsetY), 0, (scale * HEIGHT) * 2, 0, CANVAS_HEIGHT);
     return [x, y];
 }
 
@@ -78,82 +82,166 @@ class DualityCanvas {
                 this.canvas.parent('dual');
             }
 
-            this.canvas.mousePressed(() => this.onMousePressed());
-            this.canvas.mouseReleased(() => this.onMouseReleased());
+            this.canvas.mousePressed((e) => this.onMousePressed(e));
+            this.canvas.mouseMoved((e) => this.onMouseDragged(e));
+            this.canvas.mouseReleased((e) => this.onMouseReleased(e));
+
+            this.canvas.mouseWheel(e => {
+                const amnt = e.deltaY * 0.001;
+                scaleFactor += amnt;
+                scaleFactor = Math.max(Math.min(scaleFactor, 10.0), 0.1);
+                return false;
+            });
 
             this.sketch.stroke(0);
             this.sketch.line(this.sketch.width / 2, 0, this.sketch.width / 2, this.sketch.height);
             this.sketch.line(0, this.sketch.height / 2, this.sketch.width, this.sketch.height / 2);
+            
+            for (let element of document.getElementsByClassName("p5Canvas")) {
+                element.addEventListener("contextmenu", (e) => e.preventDefault());
+            }
         }
-    
+
         this.sketch.draw = () => this.draw();
 
         this.mouseWasPressed = false;
         this.mouseDownX = 0;
         this.mouseDownY = 0;
 
+        this.rightMouseWasPressed = false;
+        this.rightDragX = 0;
+        this.rightDragY = 0;
+
         this.isPrimal = isPrimal;
 
         this.needsDraw = [];
     }
 
-    onMousePressed() {
+    onMousePressed(e) {
         let [x, y] = canvas2coord(this.sketch.mouseX, this.sketch.mouseY);
 
-        if (!this.mouseWasPressed) {
-            this.mouseWasPressed = true;
-            this.mouseDownX = x;
-            this.mouseDownY = y;
+        switch (this.sketch.mouseButton) {
+            case this.sketch.LEFT:
+                if (!this.mouseWasPressed) {
+                    this.mouseWasPressed = true;
+                    this.mouseDownX = x;
+                    this.mouseDownY = y;
+                }
+                break;
+
+            case this.sketch.CENTER:
+                break;
+
+            case this.sketch.RIGHT:
+                if (!this.rightMouseWasPressed) {
+                    this.rightMouseWasPressed = true;
+                    this.rightDragX = this.sketch.mouseX;
+                    this.rightDragY = this.sketch.mouseY;
+                }
+                break;
         }
     }
 
-    onMouseReleased() {
+    onMouseDragged(e) {
+        if (this.rightMouseWasPressed && e.buttons === 2) {
+            targetOffsetX += map(this.sketch.mouseX - this.rightDragX, 0, 700, 0, 2*WIDTH);
+            targetOffsetY += -map(this.sketch.mouseY - this.rightDragY, 0, 700, 0, 2*HEIGHT);
+            this.rightDragX = this.sketch.mouseX;
+            this.rightDragY = this.sketch.mouseY;
+        }
+    }
+
+    onMouseReleased(e) {
         let [x, y] = canvas2coord(this.sketch.mouseX, this.sketch.mouseY);
 
-        if (this.mouseWasPressed) {
-    
-            this.mouseWasPressed = false;
+        switch (this.sketch.mouseButton) {
+            case this.sketch.LEFT:
 
-            if (euclidean([x, y], [this.mouseDownX, this.mouseDownY]) > dragThreshold) {
+                if (this.mouseWasPressed) {
 
-                if (drawSegments) {
-                    this.drawPrimitive(Primitives.Segment, this.mouseDownX, this.mouseDownY, x, y);
+                    this.mouseWasPressed = false;
 
-                    if (this.isPrimal) {
-                        dual.drawPrimitive(Primitives.Wedge, this.mouseDownX, this.mouseDownY, x, y);
+                    if (euclidean([x, y], [this.mouseDownX, this.mouseDownY]) > dragThreshold) {
+
+                        if (drawSegments) {
+                            this.drawPrimitive(Primitives.Segment, this.mouseDownX, this.mouseDownY, x, y);
+
+                            if (this.isPrimal) {
+                                dual.drawPrimitive(Primitives.Wedge, this.mouseDownX, this.mouseDownY, x, y);
+                            } else {
+                                primal.drawPrimitive(Primitives.Wedge, this.mouseDownX, this.mouseDownY, x, y);
+                            }
+                        } else {
+                            let a = (y - this.mouseDownY) / (x - this.mouseDownX);
+                            let b = 0 - (y - (a * x));
+
+                            this.drawPrimitive(Primitives.Line, a, b);
+
+                            if (this.isPrimal) {
+                                dual.drawPrimitive(Primitives.Ellipse, a, b);
+                            } else {
+                                primal.drawPrimitive(Primitives.Ellipse, a, b);
+                            }
+                        }
+
                     } else {
-                        primal.drawPrimitive(Primitives.Wedge, this.mouseDownX, this.mouseDownY, x, y);
-                    }
-                } else {
-                    let a = (y - this.mouseDownY) / (x - this.mouseDownX);
-                    let b = 0 - (y - (a * x));
-
-                    this.drawPrimitive(Primitives.Line, a, b);
-
-                    if (this.isPrimal) {
-                        dual.drawPrimitive(Primitives.Ellipse, a, b);
-                    } else {
-                        primal.drawPrimitive(Primitives.Ellipse, a, b);
+                        // Didn't drag, draw point
+                        this.drawPrimitive(Primitives.Ellipse, x, y);
+                        if (this.isPrimal) {
+                            dual.drawPrimitive(Primitives.Line, x, y);
+                        } else {
+                            primal.drawPrimitive(Primitives.Line, x, y);
+                        }
                     }
                 }
+                break;
 
-            } else {
-                // Didn't drag, draw point
-                this.drawPrimitive(Primitives.Ellipse, x, y);
-                if (this.isPrimal) {
-                    dual.drawPrimitive(Primitives.Line, x, y);
-                } else {
-                    primal.drawPrimitive(Primitives.Line, x, y);
-                }
-            }
+            case this.sketch.CENTER:
+                break;
+
+            case this.sketch.RIGHT:
+                this.rightMouseWasPressed = false;
+                break;
         }
+
     }
 
     drawPrimitive(type, ...args) {
         this.needsDraw.push(new Primitive(type, ...args));
     }
 
+    drawGrid() {
+        this.sketch.stroke(0);
+
+        const [yAxisP1X, yAxisP1Y] = coord2canvas(0, -(HEIGHT * 10));
+        const [yAxisP2X, yAxisP2Y] = coord2canvas(0, (HEIGHT * 10));
+        this.sketch.line(yAxisP1X, yAxisP1Y, yAxisP2X, yAxisP2Y);
+
+        const [xAxisP1X, xAxisP1Y] = coord2canvas(-(WIDTH * 10), 0);
+        const [xAxisP2X, xAxisP2Y] = coord2canvas((WIDTH * 10), 0);
+        this.sketch.line(xAxisP1X, xAxisP1Y, xAxisP2X, xAxisP2Y);
+
+        for (let i = -(WIDTH * 10); i < (WIDTH * 10); i++) {
+            const [x1, y1] = coord2canvas(i, -0.1);
+            const [x2, y2] = coord2canvas(i, 0.1);
+            this.sketch.line(x1, y1, x2, y2);
+        }
+
+        for (let i = -(HEIGHT * 10); i < (HEIGHT * 10); i++) {
+            const [x1, y1] = coord2canvas(-0.1, i);
+            const [x2, y2] = coord2canvas(0.1, i);
+            this.sketch.line(x1, y1, x2, y2);
+        }
+    }
+
     draw() {
+        this.sketch.clear();
+
+        offsetX = this.sketch.lerp(offsetX, targetOffsetX, 0.1);
+        offsetY = this.sketch.lerp(offsetY, targetOffsetY, 0.1);
+
+        this.drawGrid();
+
         this.sketch.stroke(0);
         this.sketch.fill(0);
         for (let i = 0; i < this.needsDraw.length; i++) {
@@ -179,8 +267,8 @@ class DualityCanvas {
 
                 let [a, b] = p.args;
 
-                let p1 = [(-200 + b) / a, -200];
-                let p2 = [(200 + b) / a, 200];
+                let p1 = [(-(WIDTH * 10) + b) / a, -(WIDTH * 10)];
+                let p2 = [((WIDTH * 10) + b) / a, (WIDTH * 10)];
 
                 let p1Canvas = coord2canvas(p1[0], p1[1]);
                 let p2Canvas = coord2canvas(p2[0], p2[1]);
@@ -191,11 +279,11 @@ class DualityCanvas {
 
                 let [a1, b1, a2, b2] = p.args;
 
-                let bLine1 = coord2canvas((-200 + b1) / a1, -200);
-                let tLine1 = coord2canvas((200 + b1) / a1, 200);
+                let bLine1 = coord2canvas((-(WIDTH * 10) + b1) / a1, -(WIDTH * 10));
+                let tLine1 = coord2canvas(((WIDTH * 10) + b1) / a1, (WIDTH * 10));
 
-                let bLine2 = coord2canvas((-200 + b2) / a2, -200);
-                let tLine2 = coord2canvas((200 + b2) / a2, 200);
+                let bLine2 = coord2canvas((-(WIDTH * 10) + b2) / a2, -(WIDTH * 10));
+                let tLine2 = coord2canvas(((WIDTH * 10) + b2) / a2, (WIDTH * 10));
 
                 this.sketch.line(bLine1[0], bLine1[1], tLine1[0], tLine1[1]);
                 this.sketch.line(bLine2[0], bLine2[1], tLine2[0], tLine2[1]);
@@ -212,7 +300,7 @@ class DualityCanvas {
                     this.sketch.vertex(...tLine2);
                     this.sketch.vertex(...intersection_canvas);
                     this.sketch.endShape();
-    
+
                     this.sketch.beginShape();
                     this.sketch.vertex(...tLine1);
                     this.sketch.vertex(...bLine2);
@@ -224,7 +312,7 @@ class DualityCanvas {
                     this.sketch.vertex(...tLine2);
                     this.sketch.vertex(...intersection_canvas);
                     this.sketch.endShape();
-    
+
                     this.sketch.beginShape();
                     this.sketch.vertex(...bLine1);
                     this.sketch.vertex(...bLine2);
@@ -234,7 +322,6 @@ class DualityCanvas {
                 this.sketch.stroke(0);
             }
         }
-        this.needsDraw = [];
     }
 }
 
